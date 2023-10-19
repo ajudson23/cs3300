e5cfedbb4f134d82de0be1515fb281463234dabc
ajudson23 <judslabs@icloud.com>
2023-10-19 12:01:21 -0600

    functional app w/ GET & POST working

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
 #path function defines a url pattern
 #'' is empty to represent based path to app
 # views.index is the function defined in views.py
 # name='index' parameter is to dynamically create url
 # example in html <a href="{% url 'index' %}">Home</a>.
+
+urlpatterns = [
 path('', views.index, name='index'),
 path('students/', views.StudentListView.as_view(), name= 'students'),
 path('student/<int:pk>', views.StudentDetailView.as_view(), name='student-detail'),
